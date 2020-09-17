f338e49e78bcde719ab30c36a6722b094338baf6
Tobias <tobi5457@edu.easj.dk>
2020-09-17 11:15:10 +0200

    56:53
    Collision detection with bricks. Alt virker.

--- js/ball.js
+++ js/ball.js
@@ -1,3 +1,5 @@
+import { detectCollision } from './collisionDetection.js';
+
 export default class Ball {
 
     constructor(game){
@@ -39,17 +41,7 @@ export default class Ball {
             this.speed.y = -this.speed.y;
         }
 
-        // collision with paddle
-        let bottomOfBall = this.position.y + this.size;
-        let topOfPaddle = this.game.paddle.position.y;
-
-        let leftSideOfPaddle = this.game.paddle.position.x;
-        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
-
-        if(bottomOfBall >= topOfPaddle
-            && this.position.x >= leftSideOfPaddle
-            && this.position.x + this.size <= rightSideOfPaddle
-        ) {
+         if(detectCollision(this, this.game.paddle)) {
             this.speed.y = -this.speed.y;
             this.position.y = this.game.paddle.position.y - this.size;
         }
