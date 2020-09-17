d30ad1c64463b44ecb9082a15fc513d38f8c2ebd
Tobias <tobi5457@edu.easj.dk>
2020-09-17 09:40:26 +0200

    36:44
    Refactoring. Alt virker.

---
+++ js/game.js
@@ -0,0 +1,29 @@
+import Paddle from "./paddle.js";
+import Ball from "./ball.js";
+import InputHandler from "./input.js";
+
+export default class Game {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+    }
+
+    start() {
+        this.ball = new Ball(this);
+        this.paddle = new Paddle(this);
+
+        new InputHandler(this.paddle);
+    }
+
+
+
+    update(deltaTime) {
+        this.paddle.update(deltaTime);
+        this.ball.update(deltaTime);
+    }
+
+    draw(ctx) {
+        this.paddle.draw(ctx);
+        this.ball.draw(ctx);
+    }
+}
