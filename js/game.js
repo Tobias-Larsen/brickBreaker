f660107a2ed5c26780ae0d95ee0c1f17bfb0801a
Tobias <tobi5457@edu.easj.dk>
2020-09-17 12:22:07 +0200

    1:16:0
    New level. Alt virker. Tutorial færdig.

--- js/game.js
+++ js/game.js
@@ -3,13 +3,14 @@ import Ball from "./ball.js";
 import InputHandler from "./input.js";
 import Brick from "./brick.js";
 
-import {buildLevel, level1} from "./levels.js";
+import {buildLevel, level1, level2} from "./levels.js";
 
 const GAMESTATE = {
     PAUSED: 0,
     RUNNING: 1,
     MENU: 2,
-    GAMEOVER: 3
+    GAMEOVER: 3,
+    NEWLEVEL: 4
 };
 
 export default class Game {
@@ -20,18 +21,23 @@ export default class Game {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
         this.gameObjects = [];
+        this.bricks = [];
         this.lives = 3;
+
+        this.levels = [level1, level2];
+        this.currentLevel = 0;
+
         new InputHandler(this.paddle, this);
     }
 
     start() {
-        if(this.gamestate !== GAMESTATE.MENU) return;
+        if(this.gamestate !== GAMESTATE.MENU && this.gamestate !== GAMESTATE.NEWLEVEL) return;
 
-        let bricks = buildLevel(this, level1);
+        this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+        this.ball.reset();
         this.gameObjects = [
             this.ball,
-            this.paddle,
-            ...bricks
+            this.paddle
         ];
 
         this.gamestate = GAMESTATE.RUNNING;
@@ -49,15 +55,21 @@ export default class Game {
         )
             return;
 
-        this.gameObjects.forEach(object => object.update(deltaTime));
+        if(this.bricks.length === 0) {
+            this.currentLevel ++;
+            this.gamestate = GAMESTATE.NEWLEVEL;
+            this.start();
+        }
 
-        this.gameObjects = this.gameObjects.filter(
-            object => !object.markedForDeletion
+        [...this.gameObjects, ...this.bricks].forEach(object => object.update(deltaTime));
+
+        this.bricks = this.bricks.filter(
+            brick => !brick.markedForDeletion
         );
     }
 
     draw(ctx) {
-        this.gameObjects.forEach(object => object.draw(ctx));
+        [...this.gameObjects, ...this.bricks].forEach(object => object.draw(ctx));
 
         //Pauseskærm
         if (this.gamestate === GAMESTATE.PAUSED) {
@@ -97,7 +109,7 @@ export default class Game {
     }
 
     togglePause()  {
-        if(this.gamestate == GAMESTATE.PAUSED) {
+        if(this.gamestate === GAMESTATE.PAUSED) {
             this.gamestate = GAMESTATE.RUNNING
         } else {
             this.gamestate = GAMESTATE.PAUSED;
